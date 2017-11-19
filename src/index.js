dd010225e056a4d91f5abad65aa9c9ee766c5f75
Dave Pearson <davep@davep.org>
2017-11-19 15:46:45 +0000

    Re-write cell styles as mixins to stop repeating things so much

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
 // Import modules.
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./styles/css/index.css";
 
 //////////////////////////////////////////////////////////////////////
 // Constants.
